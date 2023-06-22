942f35de59c292b262356e3da554e6921a18251c
loureiroeduarda <adv.eduradaloureiro@gmail.com>
2023-06-22 15:53:16 -0300

    User interaction created

--- ExerciseDataEmployee/Program.cs
+++ ExerciseDataEmployee/Program.cs
@@ -8,7 +8,17 @@
             (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o salário bruto é 
             afetado pela porcentagem) e mostrar novamente os dados do funcionário. Use a classe projetada abaixo.*/
 
+            Employee employee = new Employee();
+
+            Console.WriteLine("Entre com o nome do funcionário: ");
+            employee.Name = Console.ReadLine();
+            Console.WriteLine("Entre com o salário bruto: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor do imposto: ");
+            employee.Tax = double.Parse(Console.ReadLine());
+
             
+
         }
     }
 }
