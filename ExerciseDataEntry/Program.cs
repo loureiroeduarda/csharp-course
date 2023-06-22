9ee643b6a1f6253c1c5a684c2f1a1b5b22131378
Eduarda Loureiro <eduarda.loureiro@eldorado.org.br>
2023-06-21 21:03:24 -0300

    Data entry logic review exercise

---
+++ ExerciseDataEntry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace ExerciseDataEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quanto quartos tem na sua casa?");
+            int quantityRooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] vector = Console.ReadLine().Split(' ');
+            string lastName = vector[0];
+            int age = int.Parse(vector[1]);
+            double height = double.Parse(vector[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(name);
+            Console.WriteLine(quantityRooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
