b6f5c7369f12595ea153397fadc12bcb0273cc2c
loureiroeduarda <adv.eduradaloureiro@gmail.com>
2023-06-22 15:39:54 -0300

    Created employee class and attributes

---
+++ ExerciseDataEmployee/Employee.cs
@@ -0,0 +1,11 @@
+namespace ExerciseDataEmployee
+{
+    public class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        
+    }
+}
--- ExerciseDataEmployee/Program.cs
+++ ExerciseDataEmployee/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em seguida, mostrar os dados do funcionário
+            (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o salário bruto é 
+            afetado pela porcentagem) e mostrar novamente os dados do funcionário. Use a classe projetada abaixo.*/
+
+            
         }
     }
 }
