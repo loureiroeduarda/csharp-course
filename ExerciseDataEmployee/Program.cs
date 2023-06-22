2770191187cf814d4585feab0c7b9714610f0a5e
loureiroeduarda <adv.eduradaloureiro@gmail.com>
2023-06-22 15:34:46 -0300

    Created project

---
+++ ExerciseDataEmployee/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciseDataEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
