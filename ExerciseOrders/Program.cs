45cf62a66b815f7e8a343af5b13fe62a191035f3
loureiroeduarda <adv.eduradaloureiro@gmail.com>
2023-07-05 14:53:41 -0300

    Created project

---
+++ ExerciseOrders/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExerciseOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um sumário do pedido.
+            Nota: o instante do pedido deve ser o instante do sistema: DateTime.Now*/
+
+            
+        }
+    }
+
+}
