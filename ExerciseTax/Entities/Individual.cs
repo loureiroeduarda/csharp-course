861e551adfae380ec6effcfbfdf381d093762b90
loureiroeduarda <adv.eduradaloureiro@gmail.com>
2023-07-11 14:42:21 -0300

    Created individual class

---
+++ ExerciseTax/Entities/Individual.cs
@@ -0,0 +1,29 @@
+namespace ExerciseTax.Entities
+{
+    public class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual()
+        {
+        }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.00)
+            {
+                return AnualIncome * 0.15;
+            }
+            else
+            {
+                return AnualIncome * 0.25;
+            }
+        }
+    }
+}
+
